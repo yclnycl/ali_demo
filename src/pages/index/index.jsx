977e2e6608566faf1bd55add4b76398dd0b8d788
yuanchunlin <yuanchl@ddyx.onaliyun.com>
2022-03-01 16:06:00 +0800

    添加demo例子

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+        <contact-button tnt-inst-id="UAFZHZCN" scene="SCE01215709"  className='kefuBtn' />
+      </View>
+    )
+  }
+}
